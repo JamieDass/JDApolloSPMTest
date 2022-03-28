1da94b99ce3106f95c15f7ee2da3e1c3215834c2
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2022-03-28 12:52:16 -0700

    Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "PDFTronCollaboration",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "PDFTronCollaboration",
+            targets: ["PDFTronCollaboration"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "PDFTronCollaboration",
+	    url: "https://pdftron.s3.amazonaws.com/downloads/ios/packages/Collaboration/PDFTronCollaboration.xcframework.zip",
+	    checksum: "c95a2dc7dfed523d4c18a792ec67e0aa4af435225571f4396fbe86a77b69fc30"
+        ),
+)
