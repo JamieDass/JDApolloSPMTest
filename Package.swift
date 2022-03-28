9dbeb71dbf6bc95091acdede1c43224e0699a882
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2022-03-28 13:07:41 -0700

    Fixes

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "PDFTronCollaboration",
-	    url: "https://pdftron.s3.amazonaws.com/downloads/ios/packages/Collaboration/PDFTronCollaboration.xcframework.zip",
-	    checksum: "c95a2dc7dfed523d4c18a792ec67e0aa4af435225571f4396fbe86a77b69fc30"
+            url: "https://pdftron.s3.amazonaws.com/downloads/ios/packages/Collaboration/PDFTronCollaboration.xcframework.zip",
+            checksum: "c95a2dc7dfed523d4c18a792ec67e0aa4af435225571f4396fbe86a77b69fc30"
         )
     ]
 )
