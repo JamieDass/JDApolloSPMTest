0573148902d79a693741aaa329b4614d81679409
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2022-03-28 15:08:29 -0700

    Testing

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PDFTronCollaboration",
-            targets: ["PDFTronCollaboration"])
+            targets: ["PDFTronCollab"])
     ],
     dependencies: [
     .package(
@@ -18,11 +18,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "PDFTronCollab",
+               dependencies: [
+                .target(name: "PDFTronCollaboration"),
+                .product(name: "Apollo", package: "apollo-ios")
+               ]),
         .binaryTarget(
             name: "PDFTronCollaboration",
             url: "https://pdftron.s3.amazonaws.com/downloads/ios/packages/Collaboration/PDFTronCollaboration.xcframework.zip",
-            checksum: "c95a2dc7dfed523d4c18a792ec67e0aa4af435225571f4396fbe86a77b69fc30",
-	    dependencies: ["Apollo"]
+            checksum: "c95a2dc7dfed523d4c18a792ec67e0aa4af435225571f4396fbe86a77b69fc30"
         )
     ]
 )
